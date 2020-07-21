4d68b852cb11c4a1c74b8d1c0e152d3819b1715b
Numa <numa.maurin@etu.umontpellier.fr>
2020-07-21 15:08:05 +0200

    yes

---
+++ memory/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Card from './Card'
+import GuessCount from './GuessCount'
+import './App.css'
+
+
+class App extends Component {
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+  
+  render() {
+        return (
+          <div className="memory">
+            <GuessCount guesses={0} />
+            <Card card="😀" feedback="hidden" onClick={this.handleCardClick} /> 
+            <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+            <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+            <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+            <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+            <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+          </div>
+        )
+  }
+}
+
+
+
+export default App
